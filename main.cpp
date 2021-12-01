c8820062371e759c95907b9be2d862393d5507c6
Samuele Pelloni <91503299+samuelepellonii@users.noreply.github.com>
2021-12-01 13:21:16 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
-#include <iostream>
 
-int main(){
-  return 0;
+#include <iostream>
+#include "lib.h"
+
+using namespace std;
+
+int main() {
+    int a=0;
+    cin >> a;
+
+    numeriprimi (a);
+
+    if ( a==true ){
+        cout << "numero primo" << endl;
+    } else {
+        cout << "numero non primo" << endl;
+    }
+
+
+    return 0;
 }
