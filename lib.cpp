a2e91d8dee57b5b90ac25e59b2fd7644fdf6f490
Samuele Pelloni <91503299+samuelepellonii@users.noreply.github.com>
2021-12-01 13:20:34 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,24 @@
 
+//
+// Created by Federica on 01/12/2021.
+//
+
+#include "lib.h"
+
+bool numeriprimi ( int a ){
+    bool  vero_o_falso;
+
+    if ( a!=1 and a!=2){
+        for ( int b=2; b<a ; b++) {
+            if ( ( a % b ) == 0) {
+                vero_o_falso = false;
+                break;
+            } else {
+                vero_o_falso = true;
+            }
+        }
+    } else {
+        vero_o_falso = true;
+    }
+    return vero_o_falso;
+}
