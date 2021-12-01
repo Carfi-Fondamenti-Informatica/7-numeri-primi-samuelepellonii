55afed93ebc765f9671f07d988e5cd3b17d67da8
Samuele Pelloni <91503299+samuelepellonii@users.noreply.github.com>
2021-12-01 16:57:39 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,12 +1,10 @@
-
-
 #include "lib.h"
 
 bool numeriprimi ( int a ){
     bool  vero_o_falso;
 
-    if ( a!=1 and a!=2){
-        for ( int b=2; b<a ; b++) {
+    if ( a>2){
+        for ( long b=2; b<a ; b++) {
             if ( ( a % b ) == 0) {
                 vero_o_falso = false;
                 break;
@@ -14,8 +12,10 @@ bool numeriprimi ( int a ){
                 vero_o_falso = true;
             }
         }
-    } else {
+    } else if (a==2){
         vero_o_falso = true;
+    } else {
+        vero_o_falso = false;
     }
     return vero_o_falso;
 }
